aa722cf363e33f8d26144536d6da39c4df83a16e
tirzasrwn <tirzasrwn@gmail.com>
2025-08-22 16:20:35 +0700

    feat: Add bluetui.

--- rust_setup.sh
+++ rust_setup.sh
@@ -9,3 +9,4 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 sudo apt install cmake g++ pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 -y
 ~/.cargo/bin/cargo install alacritty
 ~/.cargo/bin/cargo install tree-sitter-cli
+~/.cargo/bin/cargo install bluetui
